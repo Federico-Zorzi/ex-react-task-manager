0e94bb2f8ad11b0322a8a2c034259cdf4706e26c
Federico-Zorzi <federico.zorzi05@gmail.com>
2025-03-12 12:44:08 +0100

    new custom hook useTask

--- src/context/taskContext.jsx
+++ src/context/taskContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import useTasks from "../custom hooks/useTasks";
 
 const TasksContext = createContext();
 
@@ -7,23 +8,7 @@ export const useTaskContext = () => useContext(TasksContext);
 
 //* export context for provider
 export const TaskContextProvider = ({ children }) => {
-  const apiUrl = import.meta.env.VITE_API_URL;
-
-  const [taskList, setTaskList] = useState();
-  /* console.log("taskList", taskList); */
-
-  useEffect(() => {
-    (async () => {
-      try {
-        const fetchTaskList = await fetch(`${apiUrl}/tasks`);
-        const taskListResponse = await fetchTaskList.json();
-        /* console.log("taskListResponse", taskListResponse); */
-        setTaskList(taskListResponse);
-      } catch (err) {
-        console.error(err);
-      }
-    })();
-  }, []);
+  const { taskList, addTask, removeTask, updateTask } = useTasks();
 
   const tasksContext = {
     test: "prova context",
---
+++ src/custom hooks/useTasks.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+const useTasks = () => {
+  const apiUrl = import.meta.env.VITE_API_URL;
+  const [taskList, setTaskList] = useState();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const fetchTaskList = await fetch(`${apiUrl}/tasks`);
+        const taskListResponse = await fetchTaskList.json();
+        /* console.log("taskListResponse", taskListResponse); */
+        setTaskList(taskListResponse);
+      } catch (err) {
+        console.error(err);
+      }
+    })();
+  }, []);
+
+  const addTask = () => {};
+  const removeTask = () => {};
+  const updateTask = () => {};
+
+  return { taskList, addTask, removeTask, updateTask };
+};
+
+export default useTasks;
